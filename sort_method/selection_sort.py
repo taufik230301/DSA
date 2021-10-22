8a23816d8a28040fe9c8a5139d5746eed0a214d4
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2021-10-22 14:02:36 +0700

    selection_sort_explained

--- sort_method/selection_sort.py
+++ sort_method/selection_sort.py
@@ -3,12 +3,14 @@ def selection_sort(number):
     for a in range(len(number)):
         # 0, 1
         min_number = a
+        # Find Minimun element
         for i in range(a+1,len(number)):
             # 4 > 7, 4>1, 7>1
             if number[min_number] > number[i]:
                 # min_number = 2, min_number = 2
                 min_number = i
         # temp = 1, temp = 7
+        # If minimun element was founded, we swap current element to minimum element
         temp = number[min_number]
         # number[2] = 4, number[2] = 7
         number[min_number] = number[a]
